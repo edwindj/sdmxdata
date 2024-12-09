fa4674f995593a7cd2e4576275ff8c204d06020d
Edwin de Jonge <edwindjonge@gmail.com>
2024-12-09 10:07:51 +0100

    wip

--- R/sdmx_parse_data_url.R
+++ R/sdmx_parse_data_url.R
@@ -1,5 +1,5 @@
 sdmx_parse_data_url <- function(x, resource = c("data", "metadata")){
-  x <- URLdecode(x)
+  x <- utils::URLdecode(x)
   resource <- match.arg(resource)
 
   detect <- sprintf("/%s/", resource)
@@ -8,13 +8,17 @@ sdmx_parse_data_url <- function(x, resource = c("data", "metadata")){
   stopifnot(length(x1) == 2)
 
   endpoint <- x1[1]
-  x2 <- strsplit(x1[2], "?", fixed = TRUE) |> unlist()
-  path <- x2[1]
-  params <- parse_url_query(x2[2])
 
-  parts <- c(resource, (strsplit(path, "/") |> unlist()))
+  path <- sub("\\?.*", "", x1[2])
+  query <- sub(".*\\?", "", x1[2])
+
+  params <- parse_url_query(query)
+
+  parts <-
+    c(resource, ( strsplit(path, "/") |> unlist() )) |>
+    as.list()
+
   names(parts) <- c("resource", "flowRef","key","providerRef")[seq_along(parts)]
-  parts <- as.list(parts)
 
   args <- c(parts, params)
 
@@ -30,10 +34,6 @@ sdmx_parse_data_url <- function(x, resource = c("data", "metadata")){
 }
 
 parse_url_query <- function(x){
-  if (is.null(x) || is.na(x)){
-    return(NULL)
-  }
-
   x <- strsplit(x, "&", fixed = TRUE) |> unlist()
   val <- sub(".*=", "", x) |> as.list()
   names(val) <- sub("=.*", "", x)
---
+++ inst/tinytest/test_sdmx_parse_data_url.R
@@ -0,0 +1,17 @@
+library(tinytest)
+
+q <- "https://sdmx-api.beta.cbs.nl/rest/data/NL1,DF_TESTSET_X01,1.0/?startPeriod=2010&endPeriod=2011"
+
+sdmx_parse_data_url <- cbsopendata:::sdmx_parse_data_url
+
+res <- sdmx_parse_data_url(q)
+
+res$args |> expect_equal(
+  list(
+    req = "https://sdmx-api.beta.cbs.nl/rest",
+    resource = "data",
+    flowRef = "NL1,DF_TESTSET_X01,1.0",
+    startPeriod = "2010",
+    endPeriod = "2011"
+  )
+)
