4fec010262a7c77a8eb0cf8349bf1f21df68260a
Edwin de Jonge <edwindjonge@gmail.com>
2025-01-11 19:07:49 +0100

    filterkey

--- R/get_observations.R
+++ R/get_observations.R
@@ -128,38 +128,3 @@ get_observations <- function(
 
   df
 }
-
-
-create_filter_key <- function(dims, filter_on){
-  if (is.null(filter_on)){
-    return(NULL)
-  }
-
-  nms <- names(dims) |> sQuote() |> paste(collapse = ",")
-
-  if (!is.list(filter_on) || is.null(names(filter_on))){
-    stop("'filter_on' must be a named list with at one or more of the",
-         " following dimension names:\n  ", nms,
-         call. = FALSE)
-  }
-
-  #check filter
-  chk <- names(filter_on) %in% names(dims)
-  if (!all(chk)){
-    stop("'filter_on' contains invalid dimension name(s): ",
-         paste0("'", names(filter_on)[!chk], "'", collapse = ","),
-         "\n  It must be one of the following dimension names: ", nms,
-         call. = FALSE
-    )
-  }
-
-  # TODO check codes, generate a warning when a code is not found in the codelist
-  # of a dimension
-
-  key <- lapply(names(dims), function(id){
-      paste(filter_on[[id]], collapse = "+")
-    }) |>
-    paste(collapse = ".")
-
-  key
-}
