609bbf80b16a8b96ce5100c0ad0866b9ed085a3c
Edwin de Jonge <edwindjonge@gmail.com>
2025-01-15 07:54:00 +0100

    updating

--- R/providers.R
+++ R/providers.R
@@ -4,3 +4,8 @@ PROVIDER_v2_1 <-
     LU1 = "https://lustat.statec.lu/rest/",
     NL1 = "https://sdmx-api.beta.cbs.nl/rest"
   )
+
+get_endpoint <- function(provider = getOption("cbsopendata", "NL1")){
+  provider <- match.arg(provider, choices = names(PROVIDER_v2_1))
+  sdmx_endpoint(PROVIDER_v2_1[[provider]])
+}
