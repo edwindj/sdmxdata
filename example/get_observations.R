5fad0afd8b2f639f080db5c58de39930fa0d804d
Edwin de Jonge <edwindjonge@gmail.com>
2025-03-20 11:22:00 +0100

    wip

--- example/get_observations.R
+++ example/get_observations.R
@@ -1,15 +1,12 @@
-if (interactive()) {
+dfs <- list_dataflows()
+ref <- dfs$ref[4]
 
-  dfs <- list_dataflows()
-  ref <- dfs$ref[4]
-
-  obs <- get_observations(
-    ref = ref,
-    filter_on = list(
-      "RegioS" = c("NL01"),
-      "Perioden" = c("2024MM01")
-    )
+obs <- get_observations(
+  ref = ref,
+  filter_on = list(
+    "RegioS" = c("NL01"),
+    "Perioden" = c("2024MM01")
   )
+)
 
-  obs
-}
+obs
